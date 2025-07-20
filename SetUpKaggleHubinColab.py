0619b4baaa139e663145b201232510e27dd5f1bd
Panuganti Snigdha <panugantisnigdha@gmail.com>
2025-07-20 16:20:58 +0530

    Delete SetUpKaggleHubinColab.py

--- SetUpKaggleHubinColab.py
+++
@@ -1,8 +0,0 @@
-!pip install kagglehub --upgrade
-
-import kagglehub
-
-# Download latest version of the dataset
-path = kagglehub.dataset_download("salmansajid05/oral-diseases")
-
-print("Dataset downloaded to:", path)
